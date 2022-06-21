3c41fa283840cdbd3869dad3f334a0c0822301ca
MCrux25 <misael.ortega25@gmail.com>
2022-06-21 11:35:50 -0500

    hydra scipts 21/06/22

---
+++ hydra-2022-6-21-11.3.42.js
@@ -0,0 +1,4 @@
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+s0.initVideo("https://upload.wikimedia.org/wikipedia/commons/9/9f/Tilley_Lamp_ogg.ogv")
+src(s0).invert().colorama(.4).modulate(src(s0).add(s0).thresh(),.07).diff(src(o0).pixelate().diff(src(o0).scale(.50).diff(src(o0).scale(1.15).luma(.75).hue().saturate([1.9,.99,1.01,1.1].smooth(1/4).fast(1/4)).brightness(-.5)))).out()
