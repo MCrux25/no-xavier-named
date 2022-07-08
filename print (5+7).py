b7683e44aebb2be2c10ae8c9561e13e8f85c46ef
MCrux25 <misael.ortega25@gmail.com>
2022-07-07 22:03:37 -0500

    Clase coder 070722

---
+++ print (5+7).py
@@ -0,0 +1,43 @@
+print (5+7)
+print (5%2)
+print (25**2+45+(7+25))
+print ('Mi papá me hacía sentir "tonto" cuando él volvía a hacer algo que me había pedido que yo hiciera')
+print ('Mi papá me hacía sentir "\to\nto\" cuando él volvía a hacer algo que me había pedido que yo hiciera')
+print ("""jamás
+me
+dejes""")
+print (r'Mi papá me hacía sentir "\to\nto\" cuando él volvía a hacer algo que me había pedido que yo hiciera')
+queja = "Mi papá me hacía sentir 'tonto' cuando él volvía a hacer algo que me había pedido que yo hiciera"
+queso = "Me molestaba que mi papá se enojaba mucho con nosotros cuando no ayudabamos a mamá a tener la casa limpia, pero él no era capaz de ver todotrabaji que ella hacía"
+print (queso)
+n1 = 15*48
+n2 = "jmon"
+nombre = "misael"
+print (n1)
+print (type(queso))
+#menu = input ("¿Qué desea ordenar?") #input es para solicitar un valor
+#print ("muy bien"+" "+"no tenemos"+" "+menu)
+#print ("tenemos"+" "+n2)
+#respuesta = input ("¿Le parece bien?")
+#unidades = int(input ("¿Cuántas unidades quiere?"))
+print (nombre[-1])
+print (len(queja))
+print (queja[0:15:15])
+print (queja)
+#desafio números
+#nota1 = int(input ("¿Cuál fue tu primera nota?")) * 40 / 100
+#nota2 = int(input ("¿Cuál fue tu primera nota?")) * 60 / 100
+#total = nota1 + nota2
+#print ("su nota total es "+ str(total))
+#desafio string
+cadena_1 = "moderno"
+cadena_2 = "Python"
+cadena_3 = "es un lenguaje"
+cadena_4 = "de programación"
+print (cadena_2+" "+cadena_3+" "+cadena_4+" "+cadena_1)
+palabra = "nómaj noc oseuq"
+fixed = palabra[::-1]
+carne = fixed[10:15]
+print (carne)
+print (carne[0:4])
+print (carne[0:5:2])
