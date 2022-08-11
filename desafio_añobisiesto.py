e4124758ccddd954b861038463563a7c8ac3ecd4
MCrux43 <misael.ortega25@gmail.com>
2022-08-11 10:38:50 -0500

    ejercicio terminado, optimizar

--- desafio_añobisiesto.py
+++ desafio_añobisiesto.py
@@ -3,17 +3,23 @@ os.system("cls")
 
 print ("Este programa le permitirá conocer si un año es, ha sido, o será bisiesto. En caso de que lo necesite")
 
-año = int(input("Ingrese el año:\n"))
 
-# while (año.isnumeric()) == true:
+año =  0
 def anobis(año):
-    if ((año % 4) == 0):
-        print (f'El {año} es bisiesto')
-        if ((año <100) and (año % 400) == 0):
-            print (f'El {año} es bisiesto')
-    else:
-        print (f'El {año} no es bisiesto')       
-# else:
-#     print (f'El dato ingresado no es un número, por favor ingresa un número: ')
-
+    while True:
+        año = input("Ingrese el año:\n")
+        if año.isnumeric():
+            añob = int(año)
+            if añob%4 == 0 and añob%100 != 0:
+                print (f'{añob} es bisiesto')
+                break
+            elif añob%4 == 0 and añob%100 == 0 and añob%400 == 0:
+                print (f'{añob} es bisiesto')
+                break
+            else:
+                print (f'{añob} no es bisiesto')
+                break           
+        else:
+            print (f'El dato ingresado no es un número, por favor ingresa un número')
+        
 anobis (año)
