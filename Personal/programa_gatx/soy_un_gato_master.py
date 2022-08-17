8a118d99d2c94aac691037e734aecf6901e66a4e
MCrux25 <misael.ortega25@gmail.com>
2022-08-17 13:36:08 -0500

    programa de práctica gatx

---
+++ Personal/programa_gatx/soy_un_gato_master.py
@@ -0,0 +1,18 @@
+print ("Bienvenido a este programa, con él podrá ser un gatx")
+name = input("¿Cómo te gustaría llamarte?:\n")
+sexo = input("""¿Te gustaría ser gato o gata?
+            m = macho
+            h = hembra
+            alv= te vale\n""")
+tipo = input("""Qué tipo de gatx te gustaría ser?
+            a: atigradx
+            b: negrx
+            c: egipcix (sin pela, como cholo
+            d: siamés
+            e: electrico\n""")
+personalidad = input("""¿Qué personalidad te gustaría tener?
+            1: Verguerx
+            2: Amigable
+            3: Solitarix
+            4: Jugueton(a)
+            5: Mamon(a)\n""")
