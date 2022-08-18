4d34cc3b7db842d3fd5b8af65c3aa44ad25f19a1
MCrux25 <misael.ortega25@gmail.com>
2022-08-18 18:52:24 -0500

    error pydub

--- Personal/programa_gatx/gato.py
+++ Personal/programa_gatx/gato.py
@@ -8,10 +8,17 @@ class gatx:
         self.personality = personality
  
     def maulla(self,maullidos):        
-        for i in maullidos:
-            print ("Miau")
-            from playsound import playsound
-            playsound (r'C:\Users\MisaelOrtegaCruz\Documents\Personal\no-xavier-named\Personal\programa_gatx\maullido_bebe.mp3')
+        #for i in maullidos:
+        print ("Miau "*maullidos)
+            
+        from pydub import AudioSegment
+        from pydub.playback import play
+        n = maullidos
+        audio = AudioSegment.from_mp3(r'C:\Users\MisaelOrtegaCruz\Documents\Personal\no-xavier-named\Personal\programa_gatx\maullido_bebe.mp3') #your audio file
+        play(audio * n)  #Play audio 2 times
+            
+            #from playsound import playsound
+            #playsound (r'C:\Users\MisaelOrtegaCruz\Documents\Personal\no-xavier-named\Personal\programa_gatx\maullido_bebe.mp3')
 
     
     def camina(self, pasos):
