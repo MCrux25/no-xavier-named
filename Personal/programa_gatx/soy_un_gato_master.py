1cffe9ada70ec90afa9989d0fac5e9e0a48d2233
MCrux43 <misael.ortega25@gmail.com>
2022-08-18 21:35:11 -0500

    maullido chido eso parece

--- Personal/programa_gatx/soy_un_gato_master.py
+++ Personal/programa_gatx/soy_un_gato_master.py
@@ -4,9 +4,9 @@ from gato import gatx
 print ("Bienvenido a este programa, con él podrá ser un gatx")
 name = input("¿Cómo te gustaría llamarte?:\n")
 sex = input("""¿Te gustaría ser gato o gata?
-            m = macho
-            h = hembra
-            alv= te vale\n""")
+m = macho
+h = hembra
+alv= te vale\n""")
 if sex == "m":
     sex = "macho"
 elif sex == "h":
@@ -14,11 +14,11 @@ elif sex == "h":
 elif sex == "alv":
     sex = "te vale"
 type = input("""Qué tipo de gatx te gustaría ser?
-            a: atigradx
-            b: negrx
-            c: egipcix (sin pela, como cholo
-            d: siamés
-            e: electrico\n""")
+a: atigradx
+b: negrx
+c: egipcix (sin pela, como cholo
+d: siamés
+e: electrico\n""")
 if type == "a":
     type = "atigradx"
 elif type == "b":
@@ -32,11 +32,11 @@ elif type == "d":
 elif type == "e":
     type = "electrico"
 personality = input("""¿Qué personalidad te gustaría tener?
-            1: Verguerx
-            2: Amigable
-            3: Solitarix
-            4: Jugueton(a)
-            5: Mamon(a)\n""")
+1: Verguerx
+2: Amigable
+3: Solitarix
+4: Jugueton(a)
+5: Mamon(a)\n""")
 if personality == "1":
     personality = "verguerx"
 elif personality == "2":
