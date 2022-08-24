0e188c89d1d9f6780bdec945e853dc176fe98e3c
MCrux43 <misael.ortega25@gmail.com>
2022-08-23 21:50:07 -0500

    clase coder 230822

---
+++ Coder/alumno.py
@@ -0,0 +1,30 @@
+import persona
+
+Persona = persona.Persona
+
+class Alumno (Persona):
+    def __init__(self, numEstudiante= None, id = None, nombre=None, apellido=None, edad=None, sexo=None, comision = None):
+        super().__init__(id, nombre, apellido, edad, sexo)
+        self.numEstudiante = numEstudiante
+        self.comision = comision
+    
+    def setAlumno (self):
+        with open('Coder/recursos/alumno.txt','a') as alumno:
+            alumno.write(f'{self.numEstudiante}|{self.id}|{self.comision}|\n')
+            alumno.close()
+        return True
+
+    def getAlumno(self, numEstudiante):
+        with open('Coder/recursos/alumno.txt','r') as alumnos:
+            for alumno in alumnos:
+                detalles = alumno.split('|')
+                if numEstudiante == detalles[0]:                
+                    MyPersona = Persona ()
+                    dataAlumno = MyPersona.getPersona(detalles[1])
+                    return detalles + dataAlumno
+            else:
+                alumnos.close()
+                return False
+
+
+        pass
---
+++ Coder/main.py
@@ -0,0 +1,14 @@
+# import persona
+
+# Persona = persona.Persona('1545697845','Misael','Cruz','19','M')
+# print(Persona.setPersona())
+
+# Persona = persona.Persona()
+# print (Persona.getPersona('1545697845'))
+
+import alumno
+
+Alumno = alumno.Alumno('154876154985','1545697845','Misael','Cruz','19','M','34605')
+print (Alumno.setAlumno())
+print (Alumno.setPersona())
+print (Alumno.getAlumno('154876154985'))
