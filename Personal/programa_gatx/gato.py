c59a362c06f0a5ed4a91f4c901e574dc066367b6
MCrux25 <misael.ortega25@gmail.com>
2022-08-24 15:43:48 -0500

    240822

--- Personal/programa_gatx/gato.py
+++ Personal/programa_gatx/gato.py
@@ -1,3 +1,4 @@
+from PIL import Image
 class gatx:
     def __init__(self,name,sex,type,personality):
         print (f'Generando un gatx llamadx {name}, {sex}, de tipo {type}, y con una personalidad {personality}')
@@ -15,24 +16,57 @@ class gatx:
         #maullidos = int(maullidos)
         while i <= maullidos:
             sum = sum + 1
-            i = i + 1
-            print (f"Miau {sum}")
+            i = i + 1            
             from playsound import playsound
-            playsound (r".\Personal\programa_gatx\maullido_bebe.mp3")
+            if self.personality == "amigable":    
+                print (f"Miaw {sum}")
+                playsound (r'recursos\amigable.mp3')
+            elif self.personality == "verguerx":
+                print (f"Iggggg {sum}")    
+                playsound (r'recursos\verguera.mp3')
+            elif self.personality == "solitarix":
+                print (f"Meaw {sum}")    
+                playsound (r'recursos\solitario.mp3')
+            elif self.personality == "jugueton(a)":
+                print (f"M i a u {sum}")    
+                playsound (r'recursos\jugueton.mp3')
+            elif self.personality == "mamon(a)":
+                print (f"Miau con desprecio {sum}")    
+                playsound (r'recursos\mamon.mp3')
 
     def camina(self, pasos):
-        print(f"Caminando {pasos} pasos")
+        if self.sex == "macho":            
+            print(f"Caminando {pasos} pasos")            
+            m = Image.open(r'recursos\macho.jpg')
+            m.show()
+        elif self.sex == "hembra":
+            print(f"Caminando {pasos} pasos")            
+            h = Image.open(r'recursos\hembra.jpg')
+            h.show()
+        elif self.sex == "te vale":
+            print(f"Caminando {pasos} pasos")            
+            alv = Image.open(r'recursos\alv.jpg')
+            alv = alv.convert('L')
+            alv.show()
+        
     
     def ronronea(self,ronroneo):
         sum = 0
-        i= 1
-        #ronroneo = int(ronroneo)
+        i= 1        
         while i <= ronroneo:
             sum = sum + 1
             i = i + 1
-            print (f"Prrr {sum}")
+            print (f"Prrr {sum}")            
             from playsound import playsound
-            playsound (r".\Personal\programa_gatx\gato_ronroneo.mp3")
+            playsound (r"recursos\gato_ronroneo.mp3")
 
     def dormir(self):
-        print(f"Durmiendo...")
+        import time
+        import msvcrt        
+        t0 = time.time()
+        t0 = int(t0)
+        print("DURMIENDO...")
+        msvcrt.getch()
+        t1 = time.time()
+        t1 = int(t1)
+        print(f"Dormiste por {t1-t0} segundos")
