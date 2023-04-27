ad1cc96824386f399a4349b2a2df38eecda2f515
MCrux25 <misael.ortega25@gmail.com>
2023-04-26 20:45:07 -0600

    programa para redimensionar imagenes, puede mejorar

---
+++ Personal/resize_images/resize_images.py
@@ -0,0 +1,45 @@
+import os
+from PIL import Image
+from pathlib import Path
+from tqdm import tqdm
+
+
+##sirve para definir directorio base
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+##Directorio de imagenes a reescalar
+source_path = '{BASE_DIR}/source'
+if not os.path.exists(source_path):
+    os.makedirs(source_path)
+
+##directorio de imagenes reescaladas
+desth_path = '{BASE_DIR}/img_rescaladas'
+if not os.path.exists(desth_path):
+    os.makedirs(desth_path)
+
+# Tamaño deseado de las imágenes
+nuevo_ancho = int(input('¿Cual será el nuevo ancho de las imágenes?'))
+nuevo_alto = int(input('¿Cual será el nuevo alto de las imágenes?'))
+
+def resize():
+    for filename in tqdm(os.listdir(source_path)):
+    # Comprobamos que el archivo sea una imagen en formato JPEG
+        if filename.endswith('.jpg') or filename.endswith('.jpeg'):
+            # Abrimos la imagen
+            imagen = Image.open(os.path.join(source_path, filename))
+            # Redimensionamos la imagen
+            imagen = imagen.resize((nuevo_ancho, nuevo_alto))
+            # Guardamos la nueva imagen en la carpeta de destino
+            imagen.save(os.path.join(desth_path, filename))
+
+    print ("Se han reescalado todas las imágenes")
+
+def delete_originals():
+    for filename in tqdm(os.listdir(source_path)):
+        if filename.endswith('.jpg') or filename.endswith('.jpeg'):
+            os.remove(os.path.join(source_path, filename))
+
+    print ("Se han eliminado las imágenes originales")
+
+resize ()
+delete_originals()
